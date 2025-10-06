c89be8c809ee2673da769f0814f5f3b022dbde70
Zombreveloper <73296409+Zombreveloper@users.noreply.github.com>
2025-10-06 19:48:25 +0200

    better accuracy with wavenumbers as decimals
    Also added some debug logs to check accuracy of wavelength to wavenumber and reverse conversions

--- Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/SimCalculator.cs
+++ Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/SimCalculator.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class SimCalculator : MonoBehaviour
 {
     [SerializeField] private TableImportSO m_coneTable;
+    private Sensitivities m_Sensitivities;
+
+    private float m_coneshift = 500f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Die Startwellenlänge vom Index 13 ist: " + m_coneTable.rows[13].Lambda_nm);
         var wavenumConeL = ConeAsWavenumber();
-        wavenumConeL = ShiftConeByWavenumber(wavenumConeL, -500f);
+        wavenumConeL = ShiftConeByWavenumber(wavenumConeL, m_coneshift);
+        ToWavelength(wavenumConeL);
 
     }
 
@@ -23,11 +28,11 @@ public class SimCalculator : MonoBehaviour
 
     private Sensitivities ConeAsWavenumber()
     {
-        List<float> wavenumbers = new List<float>();
+        List<decimal> wavenumbers = new List<decimal>();
         List<float> coneSensitivities = new List<float>();
 
         //conversion factor nm to cm^-1
-        var centimeter = Mathf.Pow(10, 7);
+        var centimeter = (decimal)Mathf.Pow(10, 7);
 
         foreach (var item in m_coneTable.rows)
         {
@@ -35,6 +40,7 @@ public class SimCalculator : MonoBehaviour
             coneSensitivities.Add(item.LogL2);
         }
         Sensitivities sensitivities = new Sensitivities(wavenumbers, coneSensitivities);
+        Debug.Log("Die Wavenumber von Index 13 ist: " + wavenumbers[13]);
         return sensitivities;
     }
 
@@ -42,18 +48,40 @@ public class SimCalculator : MonoBehaviour
     {
         for (int i = 0; i < sensitivity.Wavenumbers.Count; i++)
         {
-            sensitivity.Wavenumbers[i] -= shift;
+            sensitivity.Wavenumbers[i] += (decimal)shift;
         }
         return sensitivity;
+    }
+
+    private void ToWavelength(Sensitivities sens)
+    {
+        List<float> wavelengths = new List<float>();
+        var nanometer = (decimal)Mathf.Pow(10, 7);
+
+        if (sens.Wavelengths != null)
+        {
+            sens.Wavelengths.Clear();
+        }
+
+        foreach (var item in sens.Wavenumbers)
+        {
+            //wavelengths.Add(nanometer / item);
+            var value = nanometer / item;
+            wavelengths.Add((float)value);
+        }
+        sens.Wavelengths = wavelengths;
+
+        Debug.Log("Die zurücktransformierte Wellenlänge von Index 13 ist: " + sens.Wavelengths[13]);
     }
 }
 
 public class Sensitivities
 {
-    public List<float> Wavenumbers;
+    public List<decimal> Wavenumbers;
     public List<float> ConeSensitivities;
+    public List<float> Wavelengths;
 
-    public Sensitivities(List<float> wavenumbers, List<float> coneSensitivities)
+    public Sensitivities(List<decimal> wavenumbers, List<float> coneSensitivities)
     {
         this.Wavenumbers = wavenumbers;
         this.ConeSensitivities= coneSensitivities;

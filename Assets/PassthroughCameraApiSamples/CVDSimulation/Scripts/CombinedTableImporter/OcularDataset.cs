365706692925a6ce02f4f802092036093d5c1a5f
Zombreveloper <73296409+Zombreveloper@users.noreply.github.com>
2025-10-08 12:52:42 +0200

    Add Working LConeShift
    edited Ocular Dataset and SimCalculator to build a new Dataset for shifted Values independent of nm Step sizes

--- Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/CombinedTableImporter/OcularDataset.cs
+++ Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/CombinedTableImporter/OcularDataset.cs
@@ -1,13 +1,113 @@
 /* Datenstruktur, die alle für mich nötigen Werte aus mehreren Tabellen in Abhängigkeit der Wellenlänge speichert
+ * Hinweis: evt Wavenumber wegen der hohen Anzahl an Nachkommastellen als double oder decimal speichern!
+ * Macht allerdings die restlichen Funktionen bedeutend schwieriger... vllt Rundungsfehler nicht so schlimm.
+ * Ansonsten wohl oder übel alle Werte als krassere Typen speichern.
  */
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "OcularDataset", menuName = "Data/Ocular Dataset")]
 public class OcularDataset : ScriptableObject
 {
     public List<SpectralRow> rows = new List<SpectralRow>();
+
+    private void OnEnable()
+    {
+        CreateWavenumberValues();
+    }
+
+    private void CreateWavenumberValues()
+    {
+        //conversion factor nm to cm^-1
+        var centimeter = Mathf.Pow(10, 7);
+
+        foreach (var row in rows)
+        {
+            row.Wavenumber_cm_1 = centimeter / row.Lambda_nm;
+        }
+    }
+
+    public float AsWavelength(float wavenum)
+    {
+        var nanometer = Mathf.Pow(10, 7);
+        var value = nanometer / wavenum;
+        return value;
+    }
+
+    public float GetValueByWavelength(float wavelength, Func<SpectralRow, float> type)
+    {
+        //if wavelength outside accessible area get values from outmost possible point
+        if (wavelength <= rows.First().Lambda_nm)
+        { return type(rows.First()); }
+        if (wavelength >= rows.Last().Lambda_nm)
+        { return type(rows.Last()); }
+
+        //Linear interpolation between table values to make an aproximation of every point on spectrum
+        for (int i = 0; i < rows.Count - 1; i++)
+        {
+            var a = rows[i];
+            var b = rows[i + 1];
+            if (wavelength >= a.Lambda_nm && wavelength <= b.Lambda_nm)
+            {
+                float t = (wavelength - a.Lambda_nm) / (b.Lambda_nm - a.Lambda_nm);
+                return Mathf.Lerp(type(a), type(b), t);
+            }
+        }
+        //just to shut the compiler up
+        Debug.LogWarning("No Value for your wavelength could be obtained. If this message shows up something went srsly wrong!");
+        return 0;
+    }
+
+    //Same as GetValueByWavelength but with reverse comparison checks bc Length antiproportional to Number
+    public float GetValueByWavenumber(float wavenum, Func<SpectralRow, float> type)
+    {
+        //if wavelength outside accessible area get values from outmost possible point
+        if (wavenum >= rows.First().Wavenumber_cm_1)
+        { return type(rows.First()); }
+        if (wavenum <= rows.Last().Wavenumber_cm_1)
+        { return type(rows.Last()); }
+
+        //Linear interpolation between table values to make an aproximation of every point on spectrum
+        for (int i = 0; i < rows.Count - 1; i++)
+        {
+            var a = rows[i];
+            var b = rows[i + 1];
+            if (wavenum <= a.Wavenumber_cm_1 && wavenum >= b.Wavenumber_cm_1)
+            {
+                float t = (wavenum - a.Lambda_nm) / (b.Lambda_nm - a.Lambda_nm);
+                return Mathf.Lerp(type(a), type(b), t);
+            }
+        }
+        Debug.LogWarning("No Value for your wavenumber could be obtained. If this message shows up something went srsly wrong!");
+        return 0;
+    }
+
+    //Not needed anymore but kept bc may be relevant later on. 
+    //Needs to interpolate set values to fit 5nm wavelength steps
+    public void SetValueByWavenumber(float wavenum, Func<SpectralRow, float> type)
+    {
+        if (wavenum >= rows.First().Wavenumber_cm_1 || wavenum <= rows.Last().Wavenumber_cm_1)
+        {
+            Debug.LogWarning("The value you are trying to write is out of bounds!");
+            return;
+        }
+
+        for (int i = 0; i < rows.Count - 1; i++)
+        {
+            var a = rows[i];
+            var b = rows[i + 1];
+            if (wavenum <= a.Wavenumber_cm_1 && wavenum >= b.Wavenumber_cm_1)
+            {
+                float t = (wavenum - a.Lambda_nm) / (b.Lambda_nm - a.Lambda_nm);
+                //return Mathf.Lerp(type(a), type(b), t);
+            }
+        }
+
+    }
+
 }
 
 
@@ -15,7 +115,7 @@ public class OcularDataset : ScriptableObject
 public class SpectralRow
 {
     public float Lambda_nm;
-    //public float Wavenumber_cm_1;
+    public float Wavenumber_cm_1;
 
     // Tabelle Stockman & Sharp
     public float LogL2;
--- Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/SimCalculator.cs
+++ Assets/PassthroughCameraApiSamples/CVDSimulation/Scripts/SimCalculator.cs
@@ -1,31 +1,73 @@
-//using NUnit.Framework;
+/* Class that manages all calculations for new RGB-Values for CVD-Simulation
+ * At the Moment only for L-Cone. The others follow
+ */
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SimCalculator : MonoBehaviour
 {
     [SerializeField] private OcularDataset m_oculData;
+    private OcularDataset m_shiftedData;
     private Sensitivities m_sensitivities;
 
-    private float m_coneshift = 500f;
+    private float m_coneshiftL = 500f;
+    private float m_coneshiftM = 500f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //wichtig zum Initiieren und weil viele Daten gleich bleiben und alle andern im Prozess überschrieben werden.
+        //m_shiftedData = m_oculData;
+        //oder auch nicht. Ich nehme eine leere Klasse und lasse die 5nm-Steps erstmal raus zugunsten genauerer Werte
+        m_shiftedData = ScriptableObject.CreateInstance<OcularDataset>();
+
         Debug.Log("Die Startwellenlänge vom Index 13 ist: " + m_oculData.rows[13].Lambda_nm);
-        var wavenumConeL = ConeAsWavenumber();
-        wavenumConeL = ShiftConeByWavenumber(wavenumConeL, m_coneshift);
-        ToWavelength(wavenumConeL);
+        //var wavenumConeL = ConeAsWavenumber();
+        //wavenumConeL = ShiftConeByWavenumber(wavenumConeL, m_coneshiftL);
+        //ToWavelength(wavenumConeL);
+
+        //Test: Wird der richtige Wert abgehoben?
+        var myLCone = m_oculData.GetValueByWavelength(455f, r => r.LogL2);
+        Debug.Log("Wavelength 455 has an LogL Value of: " + myLCone);
+
+        //Test: Kann ich auf diese Art verschobene Zapfen abrufen?
+        //var myWavenum = m_oculData.GetValueByWavelength(455f, r => r.Wavenumber_cm_1);
+        //var myShift = myWavenum - m_coneshiftL; // (v - delta v) aus Yaguchi Formel (1)
+        //var shiftedLCone = m_oculData.GetValueByWavenumber(myShift, r => r.LogL2);
+        ShiftCone();
+        var testRow = m_shiftedData.rows[13];
+        Debug.Log("Shifted LCone has now sensibility of "
+            + testRow.LogL2 + " at Wavelength " + testRow.Lambda_nm + " and Wavenumber " + testRow.Wavenumber_cm_1);
 
     }
 
-    // Update is called once per frame
-    void Update()
+    //Hier im nächsten Schritt Argument hinzufügen, um dynamisch jeden Cone abgreifen zu können
+    //ODER ich verändere direkt alle drei Cones und schmeiße auch alle drei Shifts rein. Ich glaube, das macht mehr Sinn.
+    //Builds new Dataset for shifted Cone sensitivities
+    void ShiftCone()
     {
-        
+        var shiftrows = m_shiftedData.rows;
+        foreach (var row in m_oculData.rows)
+        {
+            var sensLStandard = row.LogL2;
+            var v = row.Wavenumber_cm_1;
+            var vShifted = v - m_coneshiftL; // (v - delta v) aus Yaguchi Formel (1)
+
+            var sRow = new SpectralRow
+            {
+                LogL2 = sensLStandard,
+                Wavenumber_cm_1 = vShifted,
+                Lambda_nm = m_shiftedData.AsWavelength(vShifted)
+            };
+            shiftrows.Add(sRow);
+
+        }
     }
 
+    #region legacy Code
     private Sensitivities ConeAsWavenumber()
     {
         List<decimal> wavenumbers = new List<decimal>();
@@ -73,17 +115,33 @@ public class SimCalculator : MonoBehaviour
 
         Debug.Log("Die zurücktransformierte Wellenlänge von Index 13 ist: " + sens.Wavelengths[13]);
     }
+    #endregion
 }
 
 public class Sensitivities
 {
     public List<decimal> Wavenumbers;
+    public List<float> WavenumFloats;
     public List<float> ConeSensitivities;
     public List<float> Wavelengths;
 
     public Sensitivities(List<decimal> wavenumbers, List<float> coneSensitivities)
     {
         this.Wavenumbers = wavenumbers;
-        this.ConeSensitivities= coneSensitivities;
+        this.ConeSensitivities = coneSensitivities;
     }
+}
+
+//Class to temporarily store shifted Cone sensitivities without fitting them into the 5nm Wavelength steps
+//Maybe useless bc I should use an empty Ocular Dataset to store additional values
+public class SensShifted
+{
+    public List<float> Wavenumbers = new List<float>();
+    public List<float> ConeSensitivities = new List<float>();
+
+    /*public SensShifted(List<float> wavenumbers, List<float> coneSensitivities)
+    {
+        this.Wavenumbers = wavenumbers;
+        this.ConeSensitivities = coneSensitivities;
+    }*/
 }

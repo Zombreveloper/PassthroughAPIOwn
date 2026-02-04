35dba7ac97357d40464444a3e03ec36435240643
Zombreveloper <73296409+Zombreveloper@users.noreply.github.com>
2026-02-04 22:38:39 +0100

    Change Opening Scene to SimView

--- .utmp/Debug/5j59i4u1/arm64-v8a/cmake_install.cmake
+++ .utmp/Debug/5j59i4u1/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/UnityProjects/PassthroughAPIOwn/Library/PramData/AndroidApplicationController/Cache/com.StickimediaProductions.ColorblindSample/gradle-project/unityLibrary/src/main/cpp
+# Install script for directory: E:/UnityProjects/PassthroughAPIOwn/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
